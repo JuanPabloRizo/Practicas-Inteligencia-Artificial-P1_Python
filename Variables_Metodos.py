54f147e9b49094f9f5c68cabf0155ecfa4259c7b
JuanPabloRizo <136943953+JuanPabloRizo@users.noreply.github.com>
2024-09-16 11:48:57 -0600

    Add files via upload

---
+++ Variables_Metodos.py
@@ -0,0 +1,17 @@
+
+#Strings y variables
+mensaje = "Practicas Primer parcial del grupo"
+grupo = 6
+print (mensaje,grupo,'E')
+nombre = 'Juan Pablo '
+apellidos = 'Rizo Riestra'
+completo = nombre + apellidos
+print ('De ' + completo + ' 22110371')
+
+#Metodos y Saltos de línea y tabulaciones
+titulo = 'inteligencia artificial'
+titulo =  titulo.upper()
+nombre = 'mauricio alejandro cabrera arellano'
+nombre =  nombre.title()
+profesor = 'PROFESOR:'
+print (titulo + '\n' + profesor.lower() + '\t' + nombre) 
