c23c8c562387c8d20d790c1a5e1bd64323caf04a
JuanPabloRizo <136943953+JuanPabloRizo@users.noreply.github.com>
2024-09-16 12:08:07 -0600

    Practica2
    Creación de operaciones aritméticas

---
+++ Operaciones.py
@@ -0,0 +1,38 @@
+# Operaciones aritméticas 
+num1 = 16  # Se asigna el valor 16 a la variable 'num1'
+num2 = 4   # Se asigna el valor 4 a la variable 'num2'
+
+# Realizando operaciones aritméticas básicas
+suma = num1 + num2  # Suma de num1 y num2
+resta = num1 - num2  # Resta de num1 y num2
+mul = num1 * num2  # Multiplicación de num1 y num2
+div = num1 / num2  # División de num1 y num2
+
+# Imprimir los operandos y resultados de las operaciones aritméticas
+print('operando 1 = ', num1, '\toperando 2 = ', num2)  # Imprime los valores de num1 y num2 con una tabulación entre ellos
+print('', suma, '\n', resta, '\n', mul, '\n', div)  # Imprime los resultados de suma, resta, multiplicación y división, 
+                                                    # cada uno en una nueva línea usando '\n'
+
+# Exponenciación
+cuadrado = num2 ** 2  # Se eleva num2 al cuadrado (num2^2)
+cubo = num2 ** 3  # Se eleva num2 al cubo (num2^3)
+alacuarta = num2 ** 4  # Se eleva num2 a la cuarta potencia (num2^4)
+
+# Imprimir los resultados de las potencias
+print('base = ', num2)  # Imprime la base, que es num2
+print('', cuadrado, '\n', cubo, '\n', alacuarta)  # Imprime las potencias cuadrada, cúbica y cuarta de num2 en nuevas líneas
+
+# Operaciones con números decimales
+num3 = 8.555  # Se asigna el valor decimal 8.555 a num3
+num4 = 16.64  # Se asigna el valor decimal 16.64 a num4
+Resultado = num3 * num4  # Se realiza la multiplicación entre num3 y num4
+
+# Imprimir los operandos decimales y el resultado de la multiplicación
+print('operando decimal 1 = ', num3, '\toperando decimal 2 = ', num4)  # Imprime num3 y num4 con tabulación entre ellos
+
+# Redondeo del resultado a distintos números de decimales
+print('Resultado 4 decimales = ', round(Resultado, 4))  # Redondea el resultado a 4 decimales
+print('Resultado 3 decimales = ', round(Resultado, 3))  # Redondea el resultado a 3 decimales
+print('Resultado 2 decimales = ', round(Resultado, 2))  # Redondea el resultado a 2 decimales
+
+
